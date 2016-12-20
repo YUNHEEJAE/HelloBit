c0db9c0d4c08c7c32e9cce300295a96b6ca13e62
camellia <camellia@DESKTOP-86B8UEI>
2016-12-20 20:04:44 +0900

    2016-12-20 20:04 SY

---
+++ src/main/java/org/kb141/domain/TakeProgramVO.java
@@ -0,0 +1,30 @@
+package org.kb141.domain;
+
+//SY
+public class TakeProgramVO {
+
+	private String sid;
+	private Integer pno;
+
+	public String getSid() {
+		return sid;
+	}
+
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+
+	public Integer getPno() {
+		return pno;
+	}
+
+	public void setPno(Integer pno) {
+		this.pno = pno;
+	}
+
+	@Override
+	public String toString() {
+		return "TakeProgram [sid=" + sid + ", pno=" + pno + "]";
+	}
+
+}
