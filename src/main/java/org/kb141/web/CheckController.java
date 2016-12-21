7676938b7fe27de031c54e6bc81d0c47559bebd2
BitCamp <BitCamp@BitCamp-PC>
2016-12-21 19:35:34 +0900

    16-12-21 19:35

--- src/main/java/org/kb141/web/CheckController.java
+++ src/main/java/org/kb141/web/CheckController.java
@@ -1,11 +1,16 @@
 package org.kb141.web;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
+import org.kb141.domain.CheckVO;
 import org.kb141.service.CheckService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +21,17 @@ public class CheckController {
 	@Inject
 	private CheckService service;
 	
+	
+	@ResponseBody
+	@GetMapping("/checklist")
+	public List<CheckVO> checkList() throws Exception{
+		logger.info("checklist Start .......");
+		
+		logger.info("CheckList : " + service.checkList());
+		
+		return service.checkList();
+		
+	}
 	
 	
 
