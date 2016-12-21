c536c97ead7624a7537d82417b34ecb23b75d475
camellia <camellia@DESKTOP-86B8UEI>
2016-12-21 17:54:06 +0900

    Check Service까지 구현 2016-12-21 17:54
    Signed-off-by: camellia <camellia@DESKTOP-86B8UEI>

---
+++ src/main/java/org/kb141/persistence/CheckDAO.java
@@ -0,0 +1,29 @@
+package org.kb141.persistence;
+
+import java.util.List;
+
+import org.kb141.domain.CheckVO;
+/**
+ * CheckDAO 클래스 입니다.
+ * @author camellia
+ *
+ */
+public interface CheckDAO {
+
+	/**
+	 * 출석하면 로그를 남겨줍니다.
+	 * @param vo
+	 * @throws Exception
+	 */
+	public void create(CheckVO vo) throws Exception;
+	
+	
+	/**
+	 * 출석 로그 리스트입니다.
+	 * @return
+	 * @throws Exception
+	 */
+	public List<CheckVO> checkList() throws Exception;
+	
+	
+}
---
+++ src/main/java/org/kb141/persistence/CheckDAOImpl.java
@@ -0,0 +1,28 @@
+package org.kb141.persistence;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.kb141.domain.CheckVO;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CheckDAOImpl implements CheckDAO {
+	@Inject
+	private SqlSession sqlSession;
+
+	private final String NAME = "org.kb141.mapper.CheckMapper.";
+
+	@Override
+	public void create(CheckVO vo) throws Exception {
+		sqlSession.insert(NAME + "create", vo);
+	}
+
+	@Override
+	public List<CheckVO> checkList() throws Exception {
+		return sqlSession.selectList(NAME + "checkList");
+	}
+
+}
