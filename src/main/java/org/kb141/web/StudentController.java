36a783d58527b2d7fdcaf981389c977d69bc82fe
dlwhd <dlwhd@DESKTOP-43TSH47>
2016-12-21 15:10:04 +0900

    16-12-21
    15:10

--- src/main/java/org/kb141/web/StudentController.java
+++ src/main/java/org/kb141/web/StudentController.java
@@ -2,12 +2,13 @@ package org.kb141.web;
 
 import javax.inject.Inject;
 
-import javax.inject.Inject;
-
+import org.kb141.domain.StudentVO;
 import org.kb141.service.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -27,8 +28,35 @@ public class StudentController {
 	private StudentService service;
 	
 	
+	@GetMapping("/jptest")
+	public void jptestGET() throws Exception{
+		logger.info("jptest Start");
+		
+	}
 	
-	123123
+	@GetMapping("/register")
+	public void registerGET(StudentVO vo) throws Exception{
+		
+		logger.info("register GET....");
+	}
+	
+	@PostMapping("/register")
+	public void registerPOST(StudentVO vo) throws Exception{
+		
+		logger.info("VO : " + vo);
+		service.register(vo);
+	}
+	
+	@GetMapping("/modify")
+	public void modifyGET(StudentVO vo) throws Exception{
+		service.modify(vo);
+	}
+	
+	@PostMapping("/remove")
+	public void removePOST(String sid) throws Exception{
+		logger.info("remove" + sid);
+		service.remove(sid);
+	}
 	
 
 	
