aa1ca8598aa4074597241c87c904cca6df6022b3
hp <hp@LAPTOP-HJPC>
2016-12-22 12:14:18 +0900

    HJ

---
+++ src/main/java/org/kb141/web/FaculityController.java
@@ -0,0 +1,13 @@
+package org.kb141.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/faculity")
+public class FaculityController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+}
