bc0acac9793dd402343e79887fe192e26f5726fb
camellia <camellia@DESKTOP-86B8UEI>
2016-12-22 10:28:06 +0900

    Program DAO, Service Test완료
    2016-12-22
    10:28
    
    Signed-off-by: camellia <camellia@DESKTOP-86B8UEI>

---
+++ src/main/java/org/kb141/service/ProgramService.java
@@ -0,0 +1,17 @@
+package org.kb141.service;
+
+import java.util.List;
+
+import org.kb141.domain.ProgramVO;
+
+public interface ProgramService {
+	public void register(ProgramVO vo);
+
+	public ProgramVO view(Integer pno);
+
+	public void modify(ProgramVO vo);
+
+	public void remove(Integer pno);
+
+	public List<ProgramVO> getProgramList();
+}
