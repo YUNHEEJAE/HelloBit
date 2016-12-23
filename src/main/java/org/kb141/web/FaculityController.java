080b00ae217ad420433a09f3b5adf13fae151c90
juyoungjung <juyoungjung@192.168.1.82>
2016-12-23 10:03:45 +0900

    Faculity Controller 2016-12-23 10:03

--- src/main/java/org/kb141/web/FaculityController.java
+++ src/main/java/org/kb141/web/FaculityController.java
@@ -1,13 +1,41 @@
 package org.kb141.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.kb141.domain.NoticeVO;
+import org.kb141.service.NoticeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/faculity")
 public class FaculityController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+	private static final Logger logger = LoggerFactory.getLogger(FaculityController.class);
+	
+	
+	@Inject
+	private NoticeService noticeService;
+	
+	
+	@GetMapping("/noticeBoard")
+	public void getNoticeBoard(Model model)throws Exception{
+		logger.info("noticeBoard called....");
+		
+		
+		model.addAttribute("list" , noticeService.getNoticeList());	
+	}
+	
+	
+	
+
+	
 }
