3d8f67de204c5c6ffa1c88afe6b044355371ef05
juyoungjung <juyoungjung@192.168.1.82>
2016-12-23 14:04:13 +0900

    imageController update ver0.1 2016-12-23 14:04

--- src/main/java/org/kb141/web/ImageController.java
+++ src/main/java/org/kb141/web/ImageController.java
@@ -3,17 +3,45 @@ package org.kb141.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 /**
  * TBL_IMAGE
  * @author KB141
  *
  */
+@RestController
 @Controller
 @RequestMapping("/image/")
 public class ImageController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ImageController.class);
+	
+	
+	@PostMapping("/registerImage")
+	public String registerImage(String sid , MultipartFile filename)throws Exception{
+		
+		logger.info("image controller called......................................................................");
+		
+		
+		
+		logger.info("sid :" + sid);
+		
+		logger.info("filename :"  + filename);
+		
+		return "success";
+		
+	
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
