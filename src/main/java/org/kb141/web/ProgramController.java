8b080f4832b7392ba19b5af8bb5d3ede77af16fa
dlwhd <dlwhd@DESKTOP-43TSH47>
2016-12-23 12:02:50 +0900

    JP Commit 16-12-23 12:02

--- src/main/java/org/kb141/web/ProgramController.java
+++ src/main/java/org/kb141/web/ProgramController.java
@@ -1,9 +1,19 @@
 package org.kb141.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.kb141.domain.ClassroomVO;
+import org.kb141.service.ClassroomService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * TBL_PROGRAM
@@ -15,8 +25,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/program/")
 public class ProgramController {
+	// CRUD allList
 	
 	private static final Logger logger = LoggerFactory.getLogger(ProgramController.class);
+	
+	@Inject
+	ClassroomService classroomservice;
+	
+	@GetMapping("/classroom")
+	public void programpage(ClassroomVO vo, Model model){
+		logger.info("program GET .....");
+	}
+	
+	@ResponseBody
+	@GetMapping(value="/classroomlist",  produces="application/json")
+	public List<ClassroomVO> getClassroomList(Model model) throws Exception{
+		logger.info("classroomlist Start......");
+		
+		List<ClassroomVO> list = new ArrayList<ClassroomVO>();
+		list = classroomservice.getClassroomList();
+		
+		logger.info("get list : " + list);
+		
+		model.addAttribute("getclassroomlist", classroomservice.getClassroomList());
+		return list;
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 }
